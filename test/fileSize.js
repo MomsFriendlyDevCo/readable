fbc0ec90996e3c1cb1a9d5433ed38e2c87d0465b
Matt Carter <m@ttcarter.com>
2019-10-17 16:09:55 +1000

    Added fallback test for fileSize

--- test/fileSize.js
+++ test/fileSize.js
@@ -4,6 +4,8 @@ var readable = require('..');
 describe('readable.fileSize()', function() {
 
 	it('should pass simple tests', ()=> {
+		expect(readable.fileSize(0)).to.equal("");
+		expect(readable.fileSize(0, {formatters: {fallback: 'empty'}})).to.equal("empty");
 		expect(readable.fileSize(1024)).to.equal("1kb");
 		expect(readable.fileSize(1536)).to.equal("1.5kb");
 		expect(readable.fileSize(1048576)).to.equal("1mb");
